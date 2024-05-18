15b2a4c90ae9cc36fb260814717210333625a979
Bartek Pacia <barpac02@gmail.com>
2024-05-18 15:06:35 +0100

    format

--- mobile/lib/features/quiz/quiz_models.dart
+++ mobile/lib/features/quiz/quiz_models.dart
@@ -112,10 +112,18 @@ final quiz = Quiz(
       question: 'Did you separated the trash and recycled today ?',
       answers: [
         Answer("Didn't take the trash out today", 0, 10, icon: 'no_trash.png'),
-        Answer("Didn't separate the trash or recycled", -20, 10,
-            icon: 'trash_out.png'),
-        Answer('Separated almost everything', 10, 10,
-            icon: 'recycle_trash.png'),
+        Answer(
+          "Didn't separate the trash or recycled",
+          -20,
+          10,
+          icon: 'trash_out.png',
+        ),
+        Answer(
+          'Separated almost everything',
+          10,
+          10,
+          icon: 'recycle_trash.png',
+        ),
         Answer('Separated everything ', 20, 10, icon: 'recycle.png'),
       ],
     ),
